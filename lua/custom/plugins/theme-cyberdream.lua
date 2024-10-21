a416557432628a6dd754ba606bcf4106ea1feb69
Mike Harris <mikeharris22@gmail.com>
2024-10-21 18:14:20 -0400

    Added lazygit and configured themes.

--- lua/custom/plugins/theme-cyberdream.lua
+++ lua/custom/plugins/theme-cyberdream.lua
@@ -1,5 +1,5 @@
--- Catppuccin is a neovim theme plugin
--- https://github.com/catppuccin/nvim
+-- Cyberdream is a neovim theme plugin
+-- hhttps://github.com/scottmckendry/cyberdream.nvim
 
 return {
   { -- You can easily change to a different colorscheme.
@@ -7,28 +7,19 @@ return {
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    'scottmckendry/cyberdream.nvim',
+    lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
     opts = function(_, opts)
-      opts.transparent_background = true
+      opts.transparent = true
+      opts.italic_comments = true
+      opts.borderless_telescope = false
     end,
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      gitgutter = true,
-      neotree = true,
-      treesitter = true,
-      lsp_trouble = true,
-      mason = true,
-      mini = true,
-      nvim_surround = true,
-    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'catppuccin-mocha', 'catppuccin-latte', 'catppuccin-frappe' or 'catppuccin-macchiato'.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'cyberdream'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
